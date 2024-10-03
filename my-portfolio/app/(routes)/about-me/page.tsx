d8681f58fb1b70a66d93be69987734f66baa6a88
indisardi99 <indisardi99@gmail.com>
2024-10-03 16:10:38 -0300

    deploy

--- my-portfolio/app/(routes)/about-me/page.tsx
+++ my-portfolio/app/(routes)/about-me/page.tsx
@@ -1,31 +1,28 @@
-"use client"
+"use client";
 
-import { Avatar } from "@/components/avatar";
 import ContainerPage from "@/components/container";
 import CounterServices from "@/components/counter-services";
 import TimeLine from "@/components/time-line";
 import TransitionPage from "@/components/transition-page";
 
 const AboutMePage = () => {
-    return (
-        <>
-            <TransitionPage />
-            <ContainerPage>
-                <Avatar />
-                <h1 className="text-2xl leading-tight text-center md:text-left md:text-5xl md:mt-10">
-                    Toda mi{' '}
-                    <span className="font-bold text-secondary">
-                        trayectoria profesional
-                    </span>
-                </h1>
+  return (
+    <>
+      <TransitionPage />
+      <ContainerPage>
+        <h1 className="text-2xl leading-tight text-center md:text-left md:text-5xl md:mt-10">
+          Toda mi{" "}
+          <span className="font-bold text-secondary">
+            trayectoria profesional
+          </span>
+        </h1>
 
-                <CounterServices />
+        <CounterServices />
 
-                <TimeLine/>
-
-            </ContainerPage>
-        </>
-    );
-}
+        <TimeLine />
+      </ContainerPage>
+    </>
+  );
+};
 
 export default AboutMePage;
